8088362c01690b6032996b4c61d7c45d4b2636b7
Slayery777 <vladislavbrus1@gmail.com>
2023-05-28 23:30:20 +0300

    addind instruction right in main_menu, new help menu, fix bug with apples out of border

--- snake.py
+++ snake.py
@@ -9,8 +9,9 @@ os_name = os.name
 
 def main(score = None):
     os.system('cls' if os_name == 'nt' else 'clear')
-    menu_buttons = [("Start Game", start_game), ("Quit", sys.exit)]
+    menu_buttons = [("Start Game", start_game),("Help", help_menu), ("Exit", sys.exit)]
     menu_buttons_len = len(menu_buttons)-1
+    
     selected_button = 0
 
     columns, lines = shutil.get_terminal_size(fallback=())
@@ -23,6 +24,8 @@ def main(score = None):
             print(f"\033[{(lines//2+i)};{columns//2-(len(button_name))//2}H>{button_name}<")
         else:
             print(f"\033[{(lines//2+i)};{columns//2-(len(button_name))//2}H {button_name} ")
+    print(f"\033[{(lines-3)};{0}H" + "Menu Controls:")
+    print(f"\033[{(lines-2)};{0}H" + "Arrow UP, Arrow DOWN, ENTER")
     print(f"\033[{(lines-1)};{0}H" + "Snake v0.2 (2023)")
     while(True):
         event = keyboard.read_event()
@@ -43,6 +46,43 @@ def main(score = None):
                 print(f"\033[{(lines//2+i)};{columns//2-(len(button_name))//2}H>{button_name}<")
             else:
                 print(f"\033[{(lines//2+i)};{columns//2-(len(button_name))//2}H {button_name} ")
+
+def help_menu():
+    os.system('cls' if os_name == 'nt' else 'clear')
+    help_articles = ["Keyboard Bindings:","Menu - arrow UP, arrow DOWN, ENTER","Snake - arrow UP,arrow DOWN,arrow LEFT,arrow RIGHT","How To Play:","Your task is to eat as many apples as possible (indicated by the symbol O)","And make sure that the snake does not touch itself."]
+    menu_buttons = [("Main menu", main)]
+    max_article_len = len(max(help_articles, key=len))
+    help_articles_len = len(help_articles)
+    menu_buttons_len = len(menu_buttons)-1
+    selected_button = 0
+    columns, lines = shutil.get_terminal_size(fallback=())
+    for i, article in enumerate(help_articles):
+        print(f"\033[{(lines//2+i)};{columns//2-max_article_len//2}H{article}")
+    for i, button in enumerate(menu_buttons):
+        button_name, button_function = button
+        if i == selected_button:
+            print(f"\033[{(lines//2+i+help_articles_len)};{columns//2-(len(button_name))//2}H>{button_name}<")
+        else:
+            print(f"\033[{(lines//2+i+help_articles_len)};{columns//2-(len(button_name))//2}H {button_name} ")
+    while(True):
+        event = keyboard.read_event()
+        columns, lines = shutil.get_terminal_size(fallback=())
+        if event.name == 'up' and event.event_type == 'up':
+            selected_button-=1
+            if selected_button < 0:
+                selected_button = menu_buttons_len
+        elif event.name == 'down' and event.event_type == 'up':
+            selected_button+=1
+            if selected_button > menu_buttons_len:
+                selected_button = 0
+        for i, button in enumerate(menu_buttons):
+            button_name, button_function = button
+            if event.name == 'enter' and event.event_type == 'up' and i == selected_button:
+                button_function()
+            if i == selected_button:
+                print(f"\033[{(lines//2+i+help_articles_len)};{columns//2-(len(button_name))//2}H>{button_name}<")
+            else:
+                print(f"\033[{(lines//2+i+help_articles_len)};{columns//2-(len(button_name))//2}H {button_name} ")
 
 def restart_menu(score):
     os.system('cls' if os_name == 'nt' else 'clear')
@@ -102,8 +142,8 @@ def start_game():
 
     snake_segments_XY = [(lines//2, columns//2)]
 
-    appleXY = [(random.randint(0, lines),random.randint(0, columns))]
-    apple = "X"
+    appleXY = [(random.randint(0, lines),random.randint(min_columns_position, max_columns_position))]
+    apple = "O"
 
     Xspeed = 1
     Yspeed = 0.6
