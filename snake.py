031f5414b8aa72f22fd5e37f692908f67e67efda
Slayery777 <vladislavbrus1@gmail.com>
2023-05-29 01:04:52 +0300

    support for changing window size start_game is active

--- snake.py
+++ snake.py
@@ -26,7 +26,7 @@ def main(score = None):
             print(f"\033[{(lines//2+i)};{columns//2-(len(button_name))//2}H {button_name} ",end='')
     print(f"\033[{(lines-2)};{0}H" + "Menu Controls:",end='')
     print(f"\033[{(lines-1)};{0}H" + "Arrow UP, Arrow DOWN, W, S, ENTER",end='')
-    print(f"\033[{(lines)};{0}H" + "Snake v0.4 (2023)",end='')
+    print(f"\033[{(lines)};{0}H" + "Snake v0.5 (2023)",end='')
     sys.stdout.flush()
     while(True):
         event = keyboard.read_event()
@@ -160,6 +160,16 @@ def start_game():
     last_segment = snake_segments_XY[-1]
 
     os.system('cls' if os_name == 'nt' else 'clear')
+
+    for i in range(lines+1):
+        print(f"\033[{i};{left_box_side_pos}H" + "█",end='')
+        print(f"\033[{i};{right_bot_side_pos}H" + "█",end='')
+        print(f"\033[{i};{left_box_side_pos-1}H" + "█",end='')
+        print(f"\033[{i};{right_bot_side_pos-1}H" + "█",end='')
+    for i in range(left_box_side_pos + 1, right_bot_side_pos):
+        print(f"\033[{top_box_side_pos};{i}H" + "█", end='')
+        print(f"\033[{bottom_box_side_pos};{i}H" + "█", end='')
+
     while(True):
         if keyboard.is_pressed('up') and last_key_direction != 'down' or keyboard.is_pressed('w') and last_key_direction != 'down' or keyboard.is_pressed('ц') and last_key_direction != 'down':
             last_key_direction = "up"
@@ -170,11 +180,22 @@ def start_game():
         elif keyboard.is_pressed('right') and last_key_direction != 'left' or keyboard.is_pressed('d') and last_key_direction != 'left' or keyboard.is_pressed('в') and last_key_direction != 'left':
             last_key_direction = "right"
         if (columns, lines) != shutil.get_terminal_size(fallback=()):
+            os.system('cls' if os_name == 'nt' else 'clear')
             columns, lines = shutil.get_terminal_size(fallback=())
             right_bot_side_pos = columns - left_box_side_pos
             bottom_box_side_pos = lines
-            min_columns_position = left_box_side_pos + 1
-            max_columns_position = right_bot_side_pos - 1
+            min_columns_position = left_box_side_pos + 3
+            max_columns_position = right_bot_side_pos - 3
+            min_lines_position = top_box_side_pos + 2
+            max_lines_position = bottom_box_side_pos - 1
+            for i in range(lines+1):
+                print(f"\033[{i};{left_box_side_pos}H" + "█",end='')
+                print(f"\033[{i};{right_bot_side_pos}H" + "█",end='')
+                print(f"\033[{i};{left_box_side_pos-1}H" + "█",end='')
+                print(f"\033[{i};{right_bot_side_pos-1}H" + "█",end='')
+            for i in range(left_box_side_pos + 1, right_bot_side_pos):
+                print(f"\033[{top_box_side_pos};{i}H" + "█", end='')
+                print(f"\033[{bottom_box_side_pos};{i}H" + "█", end='')
         if tuple(map(int, snake_segments_XY[0])) == appleXY[0]:
             appleXY = [(random.randint(min_lines_position, max_lines_position),random.randint(min_columns_position, max_columns_position))]
             # calculate the position of the new segment
@@ -223,14 +244,6 @@ def start_game():
         relative_snake_segments_XY = [(int(x), int(y)) for x, y in snake_segments_XY]
         if len(snake_segments_XY) != len(set(snake_segments_XY)):
             restart_menu(score)
-        for i in range(lines+1):
-            print(f"\033[{i};{left_box_side_pos}H" + "█",end='')
-            print(f"\033[{i};{right_bot_side_pos}H" + "█",end='')
-            print(f"\033[{i};{left_box_side_pos-1}H" + "█",end='')
-            print(f"\033[{i};{right_bot_side_pos-1}H" + "█",end='')
-        for i in range(left_box_side_pos + 1, right_bot_side_pos):
-            print(f"\033[{top_box_side_pos};{i}H" + "█", end='')
-            print(f"\033[{bottom_box_side_pos};{i}H" + "█", end='')
         print(f"\033[{lines//2};{0}HScore: {score}")
         print(f'\033[{int(last_segment[0])};{last_segment[1]}H ', end='')
         for segment in relative_snake_segments_XY:
